caf13c2f3dcee3e68e08dba29f8aade769fdcfc5
pinger <402434104@qq.com>
2020-08-14 17:26:33 +0800

    最新一次提交

---
+++ precache-manifest.7697f0694af4fc4c756d5fa1666bd1bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4ec6a56dff247df2916",
+    "url": "/simple/css/app.21254b97.css"
+  },
+  {
+    "revision": "e80b7f260b002dc16cab24e2ffdcd81a",
+    "url": "/simple/index.html"
+  },
+  {
+    "revision": "c4ec6a56dff247df2916",
+    "url": "/simple/js/app.2c727bb4.js"
+  },
+  {
+    "revision": "76b362d2a7b149e3ecd1",
+    "url": "/simple/js/chunk-vendors.8e15cad3.js"
+  },
+  {
+    "revision": "2cf2173f89ff14552588ae58b480c100",
+    "url": "/simple/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/simple/robots.txt"
+  }
+]);
